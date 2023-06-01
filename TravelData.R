0c36f4ac432ae205c066dc94295c598381b49a4f
Anthony_Chiodi <adc8wlc@gmail.com>
2023-06-01 13:06:45 -0400

    add player age data

--- TravelData.R
+++ TravelData.R
@@ -5,11 +5,21 @@ Sys.setenv(VROOM_CONNECTION_SIZE = 262144)
 '
 eval(parse(text = buffer))
 
-travel_data_1990 <- nba_travel(start_season = 1970,
-                          end_season = 1970,
-                          phase = c("RS", "PO"))
+# travel_data_1990 <- nba_travel(start_season = 1970,
+#                           end_season = 1970,
+#                           phase = c("RS", "PO"))
 
-total_distance <- sum(travel_data_1990$Distance)
+# total_distance <- sum(travel_data_1990$Distance)
 
 
-print(total_distance)
+# print(total_distance)
+
+player_metrics <- nba_player_travel(
+    start_season = 1960,
+    end_season = 1961,
+    return_home = 3,
+    team = "Boston Celtics",
+    player = "Bill Russell"
+)
+
+print(player_metrics)
